a998c62bd8b525d57f2a1edde6def79f0254eb72
RINITA SHAI <79791813+Rinita1503@users.noreply.github.com>
2021-07-21 09:15:35 +0530

    My seventeenth commit

---
+++ Ascending Order.cpp
@@ -0,0 +1,32 @@
+//Ascending Order using array
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int i,j,a,n,number[100];
+printf("Enter the value of N \n");
+scanf("%d",&n);
+
+printf("Enter the numbers \n");
+for(i=0;i<n;++i)
+scanf("%d",&number[i]);
+
+for(i=0;i<n;++i)
+{
+for(j=i+1;j<n;++j)
+{
+
+if(number[i]>number[j])
+{
+a=number[i];
+number [i]=number [j];
+number [j]=a;
+
+}
+}
+}
+
+printf("\n\nThe number arranged in ascending order given below\n");
+for(i=0;i<n;++i)
+printf("%d\n",number[i]);
+}
